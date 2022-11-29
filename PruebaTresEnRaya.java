00578504e86ab09a68dfe412bb86f772b5e0b09c
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-11-29 13:20:32 +0100

    Clase creada e inicializada

---
+++ PruebaTresEnRaya.java
@@ -0,0 +1,5 @@
+public class PruebaTresEnRaya {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TresEnRaya.java
@@ -0,0 +1,9 @@
+publi class TresEnRaya {
+    // Atributos
+    private char[][] tablero;
+
+    //Métodos
+    void crearYLlenarTablero() {
+        tablero = new char[3][3];
+    }
+}
